3eed4d334a909829994469efec70870e668bf44e
David Griffin <dgriffin@arpio.io>
2022-05-16 10:24:07 -0700

    Flesh out app code to make it much more extensible.

--- app/index.js
+++ app/index.js
@@ -1,11 +1,32 @@
-body = document.getElementsByTagName('body')[0];
-spinner = document.getElementById('loading-spinner');
-content = document.createElement('div');
-content.classList = 'centered';
-content.id = 'content';
-content.innerHTML = '<h1>CEWL Application</h1><p>Loaded!</p><br>And here is a <a' +
-    ' href="./page.html">Page</a>.'
 
+function elem(tag, props = {}, children = []) {
+    const element = document.createElement(tag);
+    Object.entries(props).forEach( ([k, v]) => {element[k] = v});
+    children.forEach(c => {
+        if (typeof (c) == 'string') {
+            element.appendChild(document.createTextNode(c));
+        } else {
+            element.appendChild(c);
+        }
+    });
+    return element;
+}
 
-body.removeChild(spinner);
-body.appendChild(content)
+function createIndexContent() {
+    return elem('div', {id: 'content', classList: ['centered']},
+        [
+            elem('h1', {innerText: 'CEWL Application'}),
+            elem('p', {innerText: 'Loaded!'}),
+            elem('p', {}, [
+                'And here is a ',
+                elem('a', {href: './page.html', innerText: 'web page'}), '.'
+            ])
+        ]);
+}
+
+function loadIndexContent() {
+    body = document.getElementsByTagName('body')[0];
+    body.removeChild(document.getElementById('loading-spinner'));
+    body.appendChild(createIndexContent())
+}
+
