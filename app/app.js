5fe38ef49ccebc167dbb5f0aa740cab1e84a3d88
David Griffin <dgriffin@arpio.io>
2022-05-16 22:29:51 -0400

    app: Simplify and generalize code, use header/footer for index page.

--- app/app.js
+++ app/app.js
@@ -1,4 +1,3 @@
-
 function elem(tag, props = {}, children = []) {
     const element = document.createElement(tag);
     Object.entries(props).forEach( ([k, v]) => {element[k] = v});
@@ -12,54 +11,46 @@ function elem(tag, props = {}, children = []) {
     return element;
 }
 
-function createIndexContent() {
-    return elem('div', {id: 'content', classList: ['centered']},
-        [
-            elem('h1', {innerText: 'CEWL Application'}),
-            elem('p', {innerText: 'Loaded!'}),
-            elem('p', {}, [
-                'And here is a ',
-                elem('a', {href: './page.html', innerText: 'web page'}), '.'
-            ])
-        ]);
-}
-
-function addHeader() {
-    const headers = document.getElementsByTagName('header');
-    if (headers) {
-        headers[0].replaceWith(
-            elem('header', {}, [
-            elem('hgroup', {}, [
-                elem('h1', {innerText: 'CEWL Application'}),
-                elem('nav', {}, [
-                    elem('a', {href: './index.html', innerText: 'Home'}),
-                    elem('a', {href: './about.html', innerText: 'About'})
-                ])
-            ])
-        ]));
+function replaceTag(name, newElement) {
+    const elements = document.getElementsByTagName(name);
+    if (elements.length == 1) {
+        elements[0].replaceWith(newElement)
+    } else {
+        console.error(`Expected to find one ${name} element, found ${elements.length}`)
     }
 }
 
-function addFooter() {
-    const footers = document.getElementsByTagName('footer');
-    if (footers) {
-        footers[0].replaceWith(
-            elem('footer', {}, [
-                elem('hgroup', {}, ['Footer, copyright'])
-            ]));
-    }
+const SEMANTIC = {
+  'header':
+     elem('header', {}, [
+       elem('hgroup', {}, [
+         elem('h1', {innerText: 'CEWL App'}),
+         elem('nav', {}, [
+           elem('a', {href: './index.html', innerText: 'Home'}),
+           elem('a', {href: './about.html', innerText: 'About'})
+     ]) ]) ]),
+  'footer':
+    elem('footer', {}, [
+      elem('hgroup', {}, ['Footer, copyright'])
+    ])
 }
 
-function appOnLoad() {
-    addHeader();
-    addFooter();
+const ROUTES = {
+  'index.html':
+    elem('article', {id: 'content'}, [
+      elem('h1', {innerText: 'CEWL Application'}),
+      elem('p', {innerText: 'Loaded!'}),
+        elem('p', {}, [ 'And here is a ',
+        elem('a', {href: './page.html', innerText: 'web page'}), '.'
+      ])
+    ])
 }
 
-function loadIndexContent() {
-    body = document.getElementsByTagName('body')[0];
-    body.removeChild(document.getElementById('loading-spinner'));
-    body.appendChild(createIndexContent())
+window.onload = function appOnLoad() {
+    Object.entries(SEMANTIC).forEach(([tag, element]) => replaceTag(tag, element));
+    Object.entries(ROUTES).forEach(([suffix, element]) => {
+        if (window.location.href.endsWith(suffix)) {
+            replaceTag('article', element);
+        }
+    })
 }
-
-
-
